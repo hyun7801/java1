04b464ddf890ddc48aa7d957c31319e7354b1306
hyun <rebantin@naver.com>
2017-09-08 12:41:01 +0900

    hello java + bro(대사 출력)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello java");
+     System.out.println("Hello java");
+
+     System.out.println("bro");
     }
 }
