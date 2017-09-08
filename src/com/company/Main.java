ed08a50d18982212f42f2fcffad6d71cbb354970
hyun <rebantin@naver.com>
2017-09-08 13:14:52 +0900

    phase 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello java");
-        System.out.println("git phase2");
+        System.out.println("git phase3");
     }
 }
