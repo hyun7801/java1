55d60ab3b556c8a9941684218eca04f6224d824c
hyun <rebantin@naver.com>
2017-09-08 12:44:05 +0900

    Revert "hello java + bro(대사 출력)"
    This reverts commit 04b464ddf890ddc48aa7d957c31319e7354b1306.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-     System.out.println("Hello java");
-
-     System.out.println("bro");
+        System.out.println("hello java");
     }
 }
